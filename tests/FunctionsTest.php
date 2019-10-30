7a4a058a6aed017ff8616c726cf4dfb9a1d5a412
Nicolò Pignatelli <nicolo.pignatelli@careship.de>
2019-10-30 15:36:07 +0100

    Fix test run through composer test

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -11,9 +11,6 @@ use function Careship\Functional\Maybe\none;
 use function Careship\Functional\Maybe\some;
 use function Careship\Functional\success_or_fail;
 
-/**
- * @group unit
- */
 final class FunctionsTest extends TestCase
 {
     /** @test */
@@ -27,7 +24,7 @@ final class FunctionsTest extends TestCase
     /** @test */
     public function success_or_fail_success_failure_error_message()
     {
-        $this->expectExceptionMessageRegExp("/^What failed: Fail message\nCaused by: Exception message/");
+        $this->expectExceptionMessageMatches("/^What failed: Fail message\nCaused by: Exception message/");
 
         $exceptionStack = new ExceptionStack(
             new \Exception('Exception message')
@@ -47,7 +44,7 @@ final class FunctionsTest extends TestCase
     /** @test */
     public function get_some_or_fail_not_found_message()
     {
-        $this->expectExceptionMessageRegExp("/^What failed: Fail message\nCaused by: Not found/");
+        $this->expectExceptionMessageMatches("/^What failed: Fail message\nCaused by: Not found/");
 
         get_some_or_fail(new Success(none()), 'Fail message');
     }
